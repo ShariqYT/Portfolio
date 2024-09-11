bcaf8e1fac49cac5a4121d3b201696b2ce2b9bf4
ShariqYT <shariqmohd737@gmail.com>
2024-09-11 12:05:47 +0530

    Added new project and improve projects codes

---
+++ components/Cards.js
@@ -0,0 +1,26 @@
+export const Cards = [
+    {
+        id: '#1',
+        title: 'CodesCloth',
+        link: 'https://codescloth.netlify.app/',
+        description: 'CodesCloth is an E-Commerce website where users/customers can buy products like T-Shirts, Mugs, Caps, Hoodies and many more. It comes with many features where it can provides best experience like Purchasing, Checkout, etc. There is a Admin page where admin can add products, promocodes and also see analysis of this website. This website is built in a way that it provides smoothness, fast and easy to use.',
+        image: '/projects/Project_1.png',
+        tags: ['React.js', 'Next.js', 'MongoDB', 'Firebase']
+    },
+    {
+        id: '#2',
+        title: 'BuyMeAChai',
+        link: 'https://buymeachaiplz.netlify.app/',
+        description: 'At BuyMeAChai, we&#39;re all about celebrating those cozy moments and sweet connections. Just like you might buy a friend a cup of coffee to show appreciation or support, here you can do the same—except with a comforting, warm chai.',
+        image: '/projects/Project_2.png',
+        tags: ['React.js', 'Next.js', 'MongoDB', 'Firebase', '+2']
+    },
+    {
+        id: '#3',
+        title: 'YouStream',
+        link: 'https://you-stream.netlify.app/',
+        description: 'YouStream is a social media platform where users can share their videos and also can share their thoughts with other users. They can also comment on videos and also can like and unlike videos. This website is built in a way that it provides smoothness, fast and easy to use.',
+        image: '/projects/Project_3.png',
+        tags: ['React.js', 'Next.js', 'MongoDB', 'Cloudinary', '+1']
+    },
+]
--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-import Project_1 from '@/public/projects/Project_1.png'
-import Project_2 from '@/public/projects/Project_2.png'
-
+import { Cards } from './Cards'
 const Projects = () => {
     return (
         <section id="Projects" className='relative min-h-screen lg:mt-0 mt-[12rem]'>
@@ -23,48 +21,28 @@ const Projects = () => {
             </div>
 
             {/* Cards */}
-            <div className='card-container dark:text-white text-dark flex flex-col lg:flex-row gap-12 text-2xl justify-center lg:items-start items-center lg:w-[75%] m-auto'>
+            <div className={`card-container dark:text-white text-dark grid lg:grid-cols-4 grid-cols-1 gap-6 lg:gap-x-52 text-2xl lg:w-[75%] mx-1 ${Cards.length >= 4 ? 'lg:mx-36':'lg:m-auto'}`}>
+                {Cards.map((card) => (
+                    <div key={card.id} className="card z-[60] backdrop-blur-md dark:bg-black/15 bg-white/15 hover:shadow-[0_0_50px_rgba(150,50,235,.4)] transition-transform duration-200 lg:hover:scale-[1.1] lg:w-96 w-[23rem] lg:h-96 h-[23rem] flex flex-col gap-4 border-4 border-purple-700 rounded-lg p-6">
 
-                <div className="card hover:shadow-[0_0_50px_rgba(150,50,235,.4)] transition-transform duration-200 lg:hover:scale-[1.1] lg:w-96 w-[23rem] lg:h-96 h-[23rem] flex flex-col gap-4 border-4 border-purple-700 rounded-lg p-6">
-                    <div className='flex items-center justify-between'>
-                        <h1>#1</h1>
-                        <h2 className='font-semibold'>CodesCloth</h2>
-                        <Link target='_blank' href="https://codescloth.netlify.app/" className='text-sm border border-purple-700 cursor-pointer hover:scale-[1.2] transition-transform duration-200 px-4 py-1 rounded-full bg-purple-700 bg-opacity-35 font-semibold'>Visit</Link>
+                        <div className='flex items-center justify-between'>
+                            <h1>{card.id}</h1>
+                            <h2 className='font-semibold'>{card.title}</h2>
+                            <Link target='_blank' href={card.link} className='text-sm border border-purple-700 cursor-pointer hover:scale-[1.2] transition-transform duration-200 px-4 py-1 rounded-full bg-purple-700 bg-opacity-35 font-semibold'>Visit</Link>
+                        </div>
+                        <div className='mx-auto'>
+                            <Image priority unoptimized className='rounded-lg border' src={card.image} alt="project1" width={300} height={300} />
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            {card.tags.map((tag) => (
+                                <p key={tag} className='text-xs border border-purple-500 bg-purple-500 bg-opacity-35 rounded-full px-2 py-1'>{tag}</p>
+                            ))}
+                        </div>
+                        <p className='text-sm overflow-y-scroll'>
+                            {card.description}
+                        </p>
                     </div>
-                    <div className='mx-auto'>
-                        <Image priority unoptimized className='rounded-lg border' src={Project_1} alt="project1" width={300} height={300} />
-                    </div>
-                    <div className='flex items-center gap-2'>
-                        <p className='text-xs border border-blue-500 bg-blue-500 bg-opacity-35 rounded-full px-2 py-1'>React.js</p>
-                        <p className='text-xs border border-gray-400 bg-black bg-opacity-35 rounded-full px-2 py-1'>Next.js</p>
-                        <p className='text-xs border border-green-500 bg-green-500 bg-opacity-35 rounded-full px-2 py-1'>MongoDB</p>
-                        <p className='text-xs border border-orange-500 bg-orange-500 bg-opacity-35 rounded-full px-2 py-1'>Firebase</p>
-                    </div>
-                    <p className='text-sm overflow-y-scroll'>
-                        CodesCloth is an E-Commerce website where users/customers can buy products like T-Shirts, Mugs, Caps, Hoodies and many more. It comes with many features where it can provides best experience like Purchasing, Checkout, etc. There is a Admin page where admin can add products, promocodes and also see analysis of this website. This website is built in a way that it provides smoothness, fast and easy to use.
-                    </p>
-                </div>
-
-                <div className="card hover:shadow-[0_0_50px_rgba(150,50,235,.4)] transition-transform duration-200 lg:hover:scale-[1.1] lg:w-96 w-[23rem] lg:h-96 h-[23rem] flex flex-col gap-4 border-4 border-purple-700 rounded-lg p-6">
-                    <div className='flex items-center justify-between'>
-                        <h1>#2</h1>
-                        <h2 className='font-semibold'>BuyMeAChai</h2>
-                        <Link target='_blank' href="https://buymeachaiplz.netlify.app/" className='transition-transform duration-200 text-sm border border-purple-700 cursor-pointer hover:scale-[1.2] px-4 py-1 rounded-full bg-purple-700 bg-opacity-35 font-semibold'>Visit</Link>
-                    </div>
-                    <div className='mx-auto'>
-                        <Image priority unoptimized className='rounded-lg border' src={Project_2} alt="project1" width={300} height={300} />
-                    </div>
-                    <div className='flex items-center gap-2'>
-                        <p className='text-xs border border-blue-500 bg-blue-500 bg-opacity-35 rounded-full px-2 py-1'>React.js</p>
-                        <p className='text-xs border border-gray-400 bg-black bg-opacity-35 rounded-full px-2 py-1'>Next.js</p>
-                        <p className='text-xs border border-green-500 bg-green-500 bg-opacity-35 rounded-full px-2 py-1'>MongoDB</p>
-                        <p className='text-xs border border-orange-500 bg-orange-500 bg-opacity-35 rounded-full px-2 py-1'>Firebase</p>
-                        <p className='text-xs border border-purple-500 bg-purple-500 bg-opacity-35 rounded-full px-2 py-1'>+2</p>
-                    </div>
-                    <p className='text-sm overflow-y-scroll'>
-                        At BuyMeAChai, we&#39;re all about celebrating those cozy moments and sweet connections. Just like you might buy a friend a cup of coffee to show appreciation or support, here you can do the same—except with a comforting, warm chai.
-                    </p>
-                </div>
+                ))}
 
             </div>
             <div className="bg-violet-600 absolute left-[10%] bottom-[10%] rounded-full w-52 h-32 blur-[150px] lg:block hidden"></div>
